6d6ff041597afac142c677e2b663da56c9d9bd1f
Ben Dickman <Ben.Dickman@connellsgroup.co.uk>
2023-12-19 15:34:56 +0000

    Add Social Icons

---
+++ src/Umbraco13Demo.Web/ViewComponents/FooterViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco13Demo.Web.ViewModels;
+
+namespace Umbraco13Demo.Web.ViewComponents;
+
+public class FooterViewComponent : ViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync(
+        IPublishedContent model)
+    {
+        var root = model.Root();
+
+        var globalSettings = root
+            ?.Siblings<ContentModels.GlobalSettings>()
+            ?.FirstOrDefault();
+
+        var vm = new FooterViewModel
+        {
+            SocialLinks = globalSettings.SocialLinks,
+        };
+
+        return View(vm);
+    }
+}
---
+++ src/Umbraco13Demo.Web/ViewModels/FooterViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Umbraco.Cms.Core.Models.PublishedContent;
+
+namespace Umbraco13Demo.Web.ViewModels;
+
+public class FooterViewModel
+{
+    public IEnumerable<SocialLink> SocialLinks { get; set; } = new List<SocialLink>();
+
+    public bool HasSocialLinks => SocialLinks?.Any() ?? false;
+}
